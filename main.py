da620be797d6e931d537aaaf0bb030017d5f01a2
jmjlacosta <jmjlacosta@gmail.com>
2025-01-17 11:21:56 -0600

    image debug

--- main.py
+++ main.py
@@ -98,8 +98,8 @@ class EKGFormOutput(BaseModel):
 
 def process_ekg_image(
     data: Annotated[EKGFormInput, Form()],
+    image: Annotated[UploadFile, File(...)],
     request: Request,
-    image: Optional[Annotated[UploadFile, File(title="File")]] = None,
 ) -> EKGFormOutput:
     """Digitize EKG image, extract signals, and provide predictions for potential diagnoses.
 
