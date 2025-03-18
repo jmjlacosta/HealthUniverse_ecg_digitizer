8b21faea66de03a859b90f7c3502f66a516379c2
jmjlacosta <jmjlacosta@gmail.com>
2025-03-17 21:17:12 -0500

    Default vals

--- main.py
+++ main.py
@@ -110,11 +110,11 @@ class EKGFormOutput(BaseModel):
 )
 
 def process_ekg_image(
-    rhythm: Annotated[Literal["Lead II", "Lead I", "Lead III", "Lead IV"], Form(...)],  # Only allow these values
-    reference_pulse: Annotated[Literal["Left", "Right"], Form(...)],
-    ekg_format: Annotated[Literal["Standard", "Cabrera"], Form(...)],
-    force_second_contour: Annotated[Literal["False", "True"], Form(...)],  # Boolean-like choices
-    scaling_factor: Annotated[Literal["10", "0", "100", "-10", "-100"], Form(...)],
+    rhythm: Annotated[Literal["Lead I", "Lead II", "Lead III", "Lead IV"], Form(...)] = "Lead II",  # Only allow these values
+    reference_pulse: Annotated[Literal["Left", "Right"], Form(...)] = "Left",
+    ekg_format: Annotated[Literal["Standard", "Cabrera"], Form(...)] = "Standard",
+    force_second_contour: Annotated[Literal["False", "True"], Form(...)] = "False",  # Boolean-like choices
+    scaling_factor: Annotated[str, Form(...)] = "10",
     image: Annotated[UploadFile, File()],
     request: Request,
 ) -> EKGFormOutput:
@@ -152,7 +152,10 @@ def process_ekg_image(
     selected_rhythm = rhythm_map[rhythm]
     rp_at_right = reference_pulse == "Right"
     cabrera = ekg_format == "Cabrera"
-    scaling_factor = int(scaling_factor)
+    try:
+        scaling_factor = int(scaling_factor)
+    except:
+        scaling_factor = 10
     second_contour = force_second_contour == "True"
     
     # Initialize the Digitizer with dynamic options
